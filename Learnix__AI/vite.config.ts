6943d6601de77d9c3afbd15ddaa755919142e3e0
suhita2k <suhidharma@gmail.com>
2026-06-13 10:15:05 +0530

    Update vite.config.ts

--- Learnix__AI/vite.config.ts
+++ Learnix__AI/vite.config.ts
@@ -8,12 +8,18 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: true,
   },
 });
